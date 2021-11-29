b59dfb0eeaddd88dc3ddd1a0d22e27c95b075e09
Cameron Brilliant <cbrilliant20@gmail.com>
2021-11-29 11:54:11 -0500

    page transitions

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let slideScene
 let pageScene
 let mouse = document.querySelector(".cursor")
 let mouseTxt = mouse.querySelector(".cursor-text")
-console.log(mouseTxt)
+let burger = document.querySelector(".burger")
 
 function animateSlides() {
   // Initialize Controller
@@ -12,7 +12,6 @@ function animateSlides() {
   const sliders = document.querySelectorAll(".slide")
   const nav = document.querySelector(".nav-header")
   // Loop over each slide
-
   sliders.forEach((slide, index, slides) => {
     const revealImg = slide.querySelector(".reveal-img")
     const img = slide.querySelector("img")
@@ -90,6 +89,89 @@ function activeCursor(e) {
   }
 }
 
+function navToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active")
+    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" })
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" })
+    gsap.to("#logo", 1, { color: "black" })
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" })
+    document.body.classList.add("hide")
+  } else {
+    e.target.classList.remove("active")
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" })
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" })
+    gsap.to("#logo", 1, { color: "white" })
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" })
+    document.body.classList.add("remove")
+  }
+}
+
+// Barba Page Transitions
+const logo = document.querySelector("#logo")
+barba.init({
+  views: [
+    {
+      namespace: "home",
+      beforeEnter() {
+        animateSlides()
+        logo.href = "./index.html"
+      },
+      beforeLeave() {
+        slideScene.destroy()
+        pageScene.destroy()
+        controller.destroy()
+      },
+    },
+    {
+      namespace: "fashion",
+      beforeEnter() {
+        logo.href = "../index.html"
+      },
+    },
+  ],
+  transitions: [
+    {
+      leave({ current, next }) {
+        let done = this.async()
+        //An Animation
+        const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } })
+        tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 })
+        tl.fromTo(
+          ".swipe",
+          0.75,
+          { x: "-100%" },
+          { x: "0%", onComplete: done },
+          "-=0.5"
+        )
+      },
+      enter({ current, next }) {
+        let done = this.async()
+        //Scroll to the top
+        window.scrollTo(0, 0)
+        //An Animation
+        const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } })
+        tl.fromTo(
+          ".swipe",
+          1,
+          { x: "0%" },
+
+          { x: "100%", stagger: 0.2, onComplete: done }
+        )
+        tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 })
+        tl.fromTo(
+          ".nav-header",
+          1,
+          { y: "-100%" },
+          { y: "0%", ease: "power2.inOut" },
+          "-=1.5"
+        )
+      },
+    },
+  ],
+})
+
+burger.addEventListener("click", navToggle)
 window.addEventListener("mousemove", cursor)
 window.addEventListener("mouseover", activeCursor)
 
